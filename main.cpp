9b0bd414b8bd2d25f95468993ea29cd5219d7d48
S4511135 <100516447+S4511135@users.noreply.github.com>
2022-03-13 12:05:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
+/*
+Nome Giulio
+Cognome Anyanwu
+Matricola S4511135
+Esercizio : Calcolare le varie Aree delle figure geometriche, inserendo a,b,c da tastiera
+
+*/
+
 #include <iostream>
 using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
+int main() 
+{
+    float A = 0, B = 0, C = 0, AreaTriangolo = 0, AreaQuadrato = 0, AreaRettangolo = 0, AreaTrapezio = 0;
+    cin>>A>>B>>C;
+	
+    AreaTriangolo = (A * B) / 2;
+	cout<<"L'AreaTriangolo e' : "<<AreaTriangolo<<endl;
+	
+    AreaQuadrato = A * A;
+	cout<<"L'AreaQuadrato e': "<<AreaQuadrato<<endl;
+	
+    AreaRettangolo = A * B;
+	cout<<"L'AreaRettangolo e': "<<AreaRettangolo<<endl;
+	
+    AreaTrapezio=(A + B) * C/2;
+	cout<<"L'AreaTrapezio e': "<< AreaTrapezio<<endl;
 
-cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+    return 0;
 }
